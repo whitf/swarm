e2b080e62fff873dff0fffa8c4f7b61fbd31d49b
whit <wfackler@gmail.com>
2020-12-07 17:09:52 -0500

    Start work on dronectl binary.

--- src/bin/dronectl.rs
+++ src/bin/dronectl.rs
@@ -1,30 +1,78 @@
 use std::env;
+use std::path::{Path, PathBuf};
+use std::process::Command;
+
+fn get_sockets() -> Vec<String> {
+
+	let pid = 509000i32;
+
+	let mut sockets: Vec<String> = Vec::new();
+
+	let socket_path = format!("/tmp/swarm_drone_{}.sock", pid);
+	let pid_path = Path::new(&socket_path);
+
+	sockets.push(pid_path.to_str().unwrap().to_string());
+
+	return sockets;
+}
+
 
 pub fn main() {
 	const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 	let title = format!("swarn drone control v.{}", VERSION);
 
-	let args: Vec<String> = env::args().collect();
+	let mut args: Vec<String> = env::args().collect();
 
 	println!();
+	println!("length of args = {}", args.len());
 
-	let cmd  = &args[1];
+	if 1 <= args.len() {
+		args.push(String::from("help"));
+	}
+
+	let cmd = &args[1];
 
 	match &cmd[..] {
+		"configure" => {
+
+		},
 		"restart" => {
 			println!("restart the daemon...");
 		},
 		"start" => {
 			println!("start the daemon...");
+
+			let mut command = Command::new("/usr/local/bin/looper");
+
+			if let Ok(child) = command.spawn() {
+				println!("child (pid = {}) is running", child.id());
+			} else {
+				println!("ls command didn't start...");
+			}
 		},
 		"status" => {
 			println!("display the status");
 		},
 		"stop" => {
-			println!("stop the daemon...");
+			println!("Shutting down the process...");
+			let paths = get_sockets();
+
+			println!("found {} socket files", paths.len());
+
+			for path in paths {
+				println!("path = {:?}", path);
+			}
+
+			/*
+			if let Some(path) = get_sockets() {
+				println!("socket path = {:?}", path);
+			} else {
+				println!("failed to find pid for process");
+			}
+			*/
 		},
-		_ => {
+		"help" | _ => {
 			println!("help info goes here");
 		},
 	}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+use procfs::process::Process;
+use std::io::{BufRead, BufReader};
+use std::os::unix::net::{UnixStream, UnixListener};
+use std::path::{Path, PathBuf};
+use std::thread;
+use std::time::Duration;
+
+fn process_command(stream: UnixStream) {
+	let stream = BufReader::new(stream);
+	for line in stream.lines() {
+		println!("received command...");
+
+		match &line.unwrap()[..] {
+			"HALT" => {
+				//shutdown signal
+				let me = Process::myself().unwrap();
+				println!("Shutting down swarm drone (pid = {}).", me.pid);
+				
+
+				// clear pid file
+				let socket_path = format!("/tmp/swarm_drone_{}.sock", me.pid);
+				let pid_path = Path::new(&socket_path);
+				let _ = std::fs::remove_file(&pid_path).unwrap();
+
+				println!();
+
+				std::process::exit(0);
+			},
+			"RESTART" => {},
+			"SYNC" => {},
+			_ => {
+				// unrecognised command
+				// log and ignore
+			}
+		}
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+
+	let me = Process::myself().unwrap();
+	let socket_path = format!("/tmp/swarm_drone_{}.sock", me.pid);
+	let listener = UnixListener::bind(socket_path).unwrap();
+
+	println!("start process pid = {}", me.pid);
+
+	for stream in listener.incoming() {
+		match stream {
+			Ok(stream) => {
+				println!("doing some stream stuff in another thread ....");
+				thread::spawn(|| process_command(stream));
+			},
+			Err(err) => {
+				println!("Error: {}", err);
+				break;
+			},
+		}
+	}
 }
