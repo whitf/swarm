083ff6d811bc78851143ad8ad3bff24fb326ad13
whit <wfackler@gmail.com>
2020-12-12 23:00:14 -0500

    Update database validation/initialization. Add dataabse versioning.

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -3,6 +3,7 @@ use rusqlite::{Connection, NO_PARAMS, params, Result};
 use std::fs;
 use std::iter::Iterator;
 use std::sync::mpsc::Sender;
+use std::thread;
 use uuid::Uuid;
 
 use crate::models::{LogType, LogMessage};
@@ -19,6 +20,7 @@ pub struct Database {
 
 impl Database {
 	pub fn verify_or_init(id: Uuid, db_dir: String, db_file: String, log_tx: Sender<LogMessage>) -> Self {
+		const DATABASE_VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 		match fs::create_dir_all(&db_dir) {
 			Err(err) => {
@@ -40,14 +42,16 @@ impl Database {
 		if count != sql::TABLE_COUNT && count != 0 {
 			log_tx.send(LogMessage::new(
 				LogType::ErrorLog,
-				format!("Database validation error: TABLE_COUNT should be 0 (empty database) or {} (fully initialized database).", sql::TABLE_COUNT)
+				format!("Database validation error: TABLE_COUNT should be 0 (empty database) or {} (fully initialized database). Found {}", sql::TABLE_COUNT, count)
 			)).unwrap();
 
 			log_tx.send(LogMessage::new(
 				LogType::SystemLog,
 				"Database validation failed. See error log.".to_string()
 			)).unwrap();
-			
+
+			println!("Database validation error: TABLE_COUNT should be 0 (empty database) or {} (fully initialized database). Found {}. Exit from fatal error.", sql::TABLE_COUNT, count);
+			std::thread::sleep(std::time::Duration::from_secs(2));
 			std::process::exit(0x0100);
 		} else if count == 0 {
 			 log_tx.send(LogMessage::new(
@@ -55,20 +59,48 @@ impl Database {
 			 	"Initializing database from empty state...".to_string()
 			 )).unwrap();
 
+			 println!("Inilizing {} database tables...", sql::CREATE_TABLES.len());
 			 for create_table_stmt in sql::CREATE_TABLES.iter() {
 			 	conn.execute(create_table_stmt, NO_PARAMS).unwrap();
 			 }
+
+			 for job_status in sql::JOB_STATUS_VALUES.iter() {
+			 	conn.execute(sql::INSERT_JOB_STATUS_VALUES, &[job_status]).unwrap();
+			 }
+
+			 conn.execute(sql::INSERT_DATABASE_VERSION, &[DATABASE_VERSION]);
 
 			 log_tx.send(LogMessage::new(
 			 	LogType::SystemLog,
 			 	format!("Database initialization complete: {} tables.", sql::TABLE_COUNT)
 			 )).unwrap();
-		} else {
+			 
+			 println!(" finished.");
+		}
+
+		let mut stmt = conn.prepare(sql::SELECT_DATABASE_VERSION).expect("Sql prepare statement failed.");
+		let database_version: String = stmt.query_row(NO_PARAMS, |row| row.get(0)).unwrap();
+
+		if database_version != DATABASE_VERSION {
+			log_tx.send(LogMessage::new(
+				LogType::ErrorLog,
+				format!("Database validation error: DATABASE_VERSION ({}) expected to be {}. Exit from fatal error.", database_version, DATABASE_VERSION)
+			)).unwrap();
+
 			log_tx.send(LogMessage::new(
 				LogType::SystemLog,
-				format!("Database validated: {} of {} tables.", count, sql::TABLE_COUNT)
+				"Database validation failed. See error log.".to_string()
 			)).unwrap();
+
+			println!("Database validation error: DATABASE_VERSION ({}) expected to be {}. Exit from fatal error.", database_version, DATABASE_VERSION);
+			std::thread::sleep(std::time::Duration::from_secs(2));
+			std::process::exit(0x0101);
 		}
+
+		log_tx.send(LogMessage::new(
+			LogType::SystemLog,
+			format!("Database v.{} validated: {} of {} tables.", DATABASE_VERSION, count, sql::TABLE_COUNT)
+		)).unwrap();
 
 		Database {
 			db_dir,
--- src/db/sql.rs
+++ src/db/sql.rs
@@ -1,25 +1,47 @@
-//use swarm::sql;
 
-pub const TABLE_COUNT: usize = 2;
-pub const FUNCTION_COUNT: usize = 0;
+pub const TABLE_COUNT: usize = 5;
 
-pub const CREATE_TABLES: [&str; 2] = [
+pub const CREATE_TABLES: [&str; 5] = [
+	CREATE_TABLE_DRONE,
 	CREATE_TABLE_JOB,
 	CREATE_TABLE_JOB_STATUS,
+	CREATE_TABLE_DRONE_OWNERSHIP,
+	CREATE_TABLE_DATABASE_VERSION,
 ];
 
-pub const SELECT_TABLE_COUNT: &str = "
-	SELECT count(*) FROM sqlite_master WHERE type = 'table' AND name != 'sqlite_sequence';
+/* Create tables. */
+pub const CREATE_TABLE_DATABASE_VERSION: &str = "
+	CREATE TABLE database_version (
+		version VARCHAR(16)
+	);
 ";
 
-/* Create tables. */
+pub const CREATE_TABLE_DRONE: &str = "
+	CREATE TABLE drone (
+		active bool NOT NULL DEFAULT true,
+		address VARCHAR(20) NOT NULL,
+		id Uuid PRIMARY KEY NOT NULL,
+		port INTEGER NOT NULL DEFAULT 9079
+	);
+";
+
+pub const CREATE_TABLE_DRONE_OWNERSHIP: &str = "
+	CREATE TABLE drone_ownership (
+		drone_id Uuid NOT NULL,
+		job_id Uuid NOT NULL,
+		PRIMARY KEY (drone_id, job_id)
+		FOREIGN KEY(drone_id) REFERENCES drone(id),
+		FOREIGN KEY(job_id) REFERENCES job(id)
+	);
+";
+
 pub const CREATE_TABLE_JOB: &str = "
 	CREATE TABLE job (
 		active bool NOT NULL DEFAULT true,
 		created TIMESTAMP DEFAULT CURRENT_TIMMESTAMP,
 		finished TIMESTAMP DEFAULT NULL,
 		id Uuid PRIMARY KEY,
-		status job_status
+		status VARCHAR(32)
 	);
 ";
 
@@ -38,6 +60,19 @@ pub const JOB_STATUS_VALUES: [&str; 5] = [
 	"Working"
 ];
 
+/* INSERT sql statements */
+pub const INSERT_JOB_STATUS_VALUES: &str = "INSERT INTO job_status_enum (job_status) VALUES(?1);";
+
+pub const INSERT_DATABASE_VERSION: &str = "INSERT INTO database_version (version) VALUES(?1);";
+
+
+
+/* SELECT sql statements */
+pub const SELECT_TABLE_COUNT: &str = "SELECT count(*) FROM sqlite_master WHERE type = 'table' AND name != 'sqlite_sequence';";
+
+pub const SELECT_DATABASE_VERSION: &str = "SELECT version FROM database_version LIMIT 1;";
+
+/* Tests */
 #[cfg(test)]
 mod tests {
 	use super::*;
